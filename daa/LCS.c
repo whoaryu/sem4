f1e2470704912bf163fae071f63bafa3ed313f9e
Aryan Shah <105057945+whoaryu@users.noreply.github.com>
2024-05-17 19:58:02 +0530

    Update LCS.c

--- daa/LCS.c
+++ daa/LCS.c
@@ -1,106 +1,63 @@
-/*Regional's CS Department is writing a spell-checker system, and you have been tasked with writing a function to determine how closely two words resemble each other. The algorithm you are to use, albeit not a very good one, is to compare the two words character by character, and count how many times the characters in a given position are the same. For instance, the words "TICK" and "TOCK" have a score of 3, since three characters (T, C, K) are the same. 
-Similarly, "CAT" and "DOG" score 0, since no letters match.
-You are given Strings A and B and you have to return an integer K indicating the score (as    
-defined above) of how closely the two match.*/
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
+int i, j, m, n;
+int c[20][20];
+char x[20], y[20], b[20][20];
 
-char s1[20],s2[20],b[20][20],x[20];
-int c[20][20],m,n,count=0;
-
-
-void LCS()
+void printLCS(int i, int j)
 {
-    
-
-    for(int i=0;i<=m;i++)
+    if (i == 0 || j == 0)
+        return;
+    if (b[i][j] == 'd')
     {
-        for(int j=0;j<=n;j++)
-        {
-            b[i][j]='.';
-        }
-    }  
+        printLCS(i - 1, j - 1);
+        printf("%c", x[i - 1]);
+    }
+    else if (b[i][j] == 'u')
+        printLCS(i - 1, j);
+    else
+        printLCS(i, j - 1);
+}
 
+void lcs()
+{
+    m = strlen(x);
+    n = strlen(y);
+    for (i = 0; i <= m; i++)
+        c[i][0] = 0;
+    for (i = 0; i <= n; i++)
+        c[0][i] = 0;
 
-    for(int i=1;i<=m;i++)
-    {
-        for(int j=1;j<=n;j++)
+    for (i = 1; i <= m; i++)
+        for (j = 1; j <= n; j++)
         {
-            if(s1[i-1]==s2[j-1])
+            if (x[i - 1] == y[j - 1])
             {
-                c[i][j]=c[i-1][j-1]+1;
-                b[i][j]='d';
+                c[i][j] = c[i - 1][j - 1] + 1;
+                b[i][j] = 'd';
             }
-            else if(c[i-1][j]>c[i][j-1])
+            else if (c[i - 1][j] >= c[i][j - 1])
             {
-                c[i][j]=c[i-1][j];
-                b[i][j]='u';
+                c[i][j] = c[i - 1][j];
+                b[i][j] = 'u';
             }
             else
             {
-                c[i][j]=c[i][j-1];
-                b[i][j]='l';                
+                c[i][j] = c[i][j - 1];
+                b[i][j] = 'l';
             }
         }
-    }   
-    
-
 }
-
-
-void PRINT_LCS(int i,int j)
+int main()
 {
-    if(i==0 || j==0)
-        return;
-    if(b[i][j]=='d')
-    {
-        PRINT_LCS(i-1,j-1);
-        x[count]=s1[i-1];   //If this was written above PRINT_LCS(int i,int j) it would print reverse order. Now its using backtracking .Hence no need to reverse order here. 
-        count++;
-    }
-    else if(b[i][j]=='u')
-    {
-        PRINT_LCS(i-1,j);
-    }
-    else
-    {
-        PRINT_LCS(i,j-1);
-    }
-}
-
-
-
-void main()
-{
-    printf("Enter sequence1:");
-    scanf("%s",s1);
-    printf("Enter sequence2:");
-    scanf("%s",s2);
-
-    m=strlen(s1);
-    n=strlen(s2);
-
-
-    LCS();
-
-    for(int i=0;i<=m;i++)
-    {
-        for(int j=0;j<=n;j++)
-        {
-            printf("%d ",c[i][j]);
-        }
-        printf("\n");
-    }
-    
-    PRINT_LCS(m,n);
-
-    printf("final ans:\n");
-    
-    for(int i=0;i<count;i++)
-    {
-        printf("%c",x[i]);
-    }
-
+    printf("Enter 1st sequence:");
+    scanf("%s", x);
+    printf("Enter 2nd sequence:");
+    scanf("%s", y);
+    printf("\nThe Longest Common Subsequence is ");
+    lcs();
+    printLCS(m, n);
+    return 0;
 }
