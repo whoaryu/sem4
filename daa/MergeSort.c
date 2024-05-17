c71b64bc059285d0c8f74af22abd0b34f30758d3
Aryan Shah <105057945+whoaryu@users.noreply.github.com>
2024-05-17 16:15:07 +0530

    Update MergeSort.c

--- daa/MergeSort.c
+++ daa/MergeSort.c
@@ -1,96 +1,65 @@
-//MERGE SORT
+#include <stdlib.h>
 #include <stdio.h>
-#define n 10000
-#include<time.h>
 
-int arr[n];
-void conquer(int low,int mid,int high)
+int n = 5;
+int a[5], b[5];
+
+void merge(int low, int mid, int high)
 {
-    int i=low;
-    int j=mid+1;
-    int k=low;
-    int B[n];
-    
-    while(i<=mid && j<=high)
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+    while (i <= mid && j <= high)
     {
-        if(arr[i]<arr[j])
+        if (a[i] < a[j])
         {
-            B[k]=arr[i];
-            i++;
-            k++;
+            b[k++] = a[i++];
         }
         else
         {
-            B[k]=arr[j];
-            j++;
-            k++;
+            b[k++] = a[j++];
         }
     }
-    
-    while(i<=mid)
+    while (i <= mid)
     {
-        B[k]=arr[i];
-        k++;
-        i++;
+        b[k++] = a[i++];
     }
-    
-    while(j<=high)
+    while (j <= high)
     {
-        B[k]=arr[j];
-        j++;
-        k++;
+        b[k++] = a[j++];
     }
-    
-    for(int m=low;m<high;m++)
+    for (i = low; i <= high; i++)
     {
-        arr[m]=B[m];
+        a[i] = b[i];
     }
-    
 }
 
-void divide(int low,int high)
+void mergeSort(int low, int high)
 {
-    int mid;
-    mid=(low+high)/2;
-    if(low<high)
+    if (low < high)
     {
-        divide(low,mid);
-        divide(mid+1,high);
-        conquer(low,mid,high);
+        int mid = (low + high) / 2;
+        mergeSort(low, mid);
+        mergeSort(mid + 1, high);
+        merge(low, mid, high);
     }
 }
-int main() {
-    int key;
-    clock_t t1,t2,t3;
-    for(int i=0;i<n;i++)
+
+int main()
+{
+    int data;
+    for (int i = 0; i < n; i++)
     {
-        arr[i]=rand();
+        printf("Enter data: ");
+        scanf("%d", &data);
+        a[i] = data;
     }
-    t1=clock();
-    divide(0,n-1);  
-    t2=clock();
-    t3=t2-t1;
-    printf("\Average Case:%ld",t3);
-   
- for(int i=0;i<n;i++)
+    mergeSort(0, n - 1);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
     {
-        arr[i]=i*12;
+        printf("%d ", a[i]);
     }
-    t1=clock();
-    divide(0,n-1);  
-    t2=clock();
-    t3=t2-t1;
-    printf("\nbest Case:%ld",t3);
-    
-    for(int i=0;i<n;i++)
-    {
-        arr[n-i]=i*12;
-    }
-    t1=clock();
-    divide(0,n-1);  
-    t2=clock();
-    t3=t2-t1;
-    printf("\nworst Case:%ld",t3);
-    
+    printf("\n");
     return 0;
 }
