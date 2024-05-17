8176f06bd400df56b26cb06f4c228f7a9ca6e2e6
Aryan Shah <105057945+whoaryu@users.noreply.github.com>
2024-05-17 19:59:25 +0530

    Update Floyd_Warshal.c

--- daa/Floyd_Warshal.c
+++ daa/Floyd_Warshal.c
@@ -1,52 +1,46 @@
-/*As a logistics manager for an e-commerce company, you are responsible for optimizing the travel times of delivery trucks moving between multiple warehouses in a city. The road network is complex, and you need to find the most time-efficient routes for all pairs of warehouses.
-Input:
-The number of warehouses in the city.
-A weighted directed graph representing the road network between warehouses.
-The weight of each edge represents the travel time between warehouses.
-Output:
-A matrix showing the shortest travel times between all pairs of warehouses.*/
-
-// floydwarshall
-
 #include <stdio.h>
-#define v 10
-void floyd_warshall(int a[v][v], int n)
+
+int a[10][10];
+
+void floyd(int a[10][10],int n)
 {
-    for (int k = 0; k < n; k++)
+    for(int k=0;k<n;k++)
     {
-        for (int i = 0; i < n; i++)
+        for(int i=0;i<n;i++)
         {
-            for (int j = 0; j < n; j++)
+            for(int j=0;j<n;j++)
             {
-                if (a[i][j] > a[i][k] + a[k][j])
+                if(a[i][j]>a[i][k]+a[k][j])
                 {
-                    a[i][j] = a[i][k] + a[k][j];
+                    a[i][j]=a[i][k]+a[k][j];
                 }
             }
         }
     }
-    printf("All Pairs Shortest Path is :\n");
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        for (int j = 0; j < n; j++)
+        for(int j=0;j<n;j++)
         {
-            printf("%d ", a[i][j]);
+            printf("%d",a[i][j]);
         }
         printf("\n");
     }
 }
-int main()
+
+void main()
 {
     int n;
-    scanf("%d", &n);
-    int a[v][v];
-    for (int i = 0; i < n; i++)
+    printf("n?");
+    scanf("%d",&n);
+
+    printf("matrix");
+    for(int i=0;i<n;i++)
     {
-        for (int j = 0; j < n; j++)
+        for(int j=0;j<n;j++)
         {
-            scanf("%d", &a[i][j]);
+            scanf("%d",&a[i][j]);
         }
     }
 
-    floyd_warshall(a, n);
+    floyd(a,n);
 }
